27489fd675f7f27d0eb8e6b04131f4384c27c227
samuckqaDev <samuckqa@hotmail.com>
2025-03-25 14:00:15 -0300

    LoadIndicator with styled components

--- app/index.tsx
+++ app/index.tsx
@@ -6,7 +6,7 @@ import {
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
-import { ActivityIndicator } from "react-native";
+import { Loading } from "@components/Loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -16,7 +16,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+      {!fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
