7111efcadafb3ff75829cfb6d20e12c83556b6c5
samuckqaDev <samuckqa@hotmail.com>
2025-03-25 20:54:49 -0300

    Adding Phosphor icons to React native

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components/native";
+import { CaretLeft } from "phosphor-react-native";
 
 export const Container = styled.View`
   inline-size: 100%;
@@ -12,3 +13,12 @@ export const Logo = styled.Image`
   inline-size: 46px;
   block-size: 55px;
 `;
+
+export const BackButton = styled.TouchableOpacity`
+  flex: 1;
+`;
+
+export const BackIcon = styled(CaretLeft).attrs(({ theme }) => ({
+  size: 32,
+  color: theme.COLORS.WHITE,
+}))``;
