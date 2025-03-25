4fe60f4446d3f31e300a1f4cf67d9219683e08aa
samuckqaDev <samuckqa@hotmail.com>
2025-03-25 13:52:28 -0300

    Adding Roboto font on app

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -3,16 +3,11 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
   background-color: ${({ theme }) => theme.COLORS.GRAY_600};
-  color: white;
+  padding: 24px;
+
 `;
 
 
-export const Title = styled.Text`
-  font-size: 24px;
-  color: white;
-`;
 
 
