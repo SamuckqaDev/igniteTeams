faf446604eb2da64ff7bbc9c0b8ea57d5fe60e88
samuckqaDev <samuckqa@hotmail.com>
2025-03-26 12:28:16 -0300

    ListEmpty Component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,25 +4,25 @@ import { Header } from "@components/Header";
 import { HighLight } from "@components/HighLight";
 import { GroupCard } from "@components/GroupCard";
 import { useState } from "react";
+import { ListEmpty } from "@components/ListEmpty";
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>([
-    "Rockeseat Guys",
-    "Ignite Guys",
-    "React Native Guys",
-    "Java Guys",
-    "Python Guys",
-  ]);
+  const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <Container>
       <Header />
       <HighLight title="Groups" subTitle="Play with your team" />
+
       <FlatList
         data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
         showsHorizontalScrollIndicator
+        contentContainerStyle={groups.length == 0 && { flex: 1 }}
+        ListEmptyComponent={
+          <ListEmpty message="How about registering the first group?" />
+        }
       />
     </Container>
   );
