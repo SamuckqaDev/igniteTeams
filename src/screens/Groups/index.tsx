7511ac1ac64c2baf22367b9e32fd29aadb5bc91e
samuckqaDev <samuckqa@hotmail.com>
2025-03-25 21:27:01 -0300

    Creating Group card and adding TouchableOpacity rest props

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,15 @@
 import { Text, View } from "react-native";
 import { Container } from "./styles";
 import { Header } from "@components/Header";
+import { HighLight } from "@components/HighLight";
+import { GroupCard } from "@components/GroupCard";
 
 export function Groups() {
   return (
     <Container>
       <Header />
+      <HighLight title="Groups" subTitle="Play with your team" />
+      <GroupCard title="Ignite Guys" onPress={() => console.log("card 1")} />
     </Container>
   );
 }
