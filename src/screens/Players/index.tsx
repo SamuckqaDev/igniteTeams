a2b2b4a3ed0e40ec2bd201a7f4786781472733ff
samuckqaDev <samuckqa@hotmail.com>
2025-03-27 19:53:28 -0300

    Horizontal FlatList

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,20 @@
 import { Header } from "@components/Header";
-import { Container, Form } from "./styles";
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 import { HighLight } from "@components/HighLight";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
+import { FlatList } from "react-native";
+import { useState } from "react";
 
 export function Players() {
+  const [team, setTeam] = useState<string>("Team 1");
+  const [players, setPlayers] = useState<string[]>([
+    "Player 1",
+    "Player 2",
+    "Player 3",
+  ]);
+
   return (
     <Container>
       <Header showBackButton />
@@ -14,7 +23,22 @@ export function Players() {
         <Input placeholder="Person's name" autoCorrect={false} />
         <ButtonIcon iconName="add" />
       </Form>
-      <Filter title="Team 1" />
+      <HeaderList>
+        <FlatList
+          data={["Team 1", "Team 2", "Team 3"]}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        />
+        <NumberOfPlayers>{players.length}</NumberOfPlayers>
+      </HeaderList>
     </Container>
   );
 }
